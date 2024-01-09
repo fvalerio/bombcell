a836c4e5738a748c285447594dc4b8eedac08009
fvalerio <valerio.francioni@hotmail.it>
2024-01-09 13:22:42 -0500

    Update bc_computeWaveformProp.m

--- ephysProperties/bc_computeWaveformProp.m
+++ ephysProperties/bc_computeWaveformProp.m
@@ -11,47 +11,43 @@ waveformBaselineWindow = NaN;
 [nPeaks, nTroughs, isSomatic, peakLocs, troughLocs, waveformDuration_peakTrough, ...
     ~, ~, ~, thisWaveform] = bc_waveformShape(templateWaveforms, ...
     thisUnit, maxChannel, ephys_sample_rate, channelPositions, baselineThresh, ...
-<<<<<<< HEAD
-    waveformBaselineWindow, minThreshDetectPeaksTroughs, 1.2, plotThis);
-=======
     waveformBaselineWindow, minThreshDetectPeaksTroughs, firstPeakRatio, plotThis);
 
 if ~isnan(nPeaks) && ~isnan(nTroughs)
->>>>>>> upstream/main
 
-% time 
-wvTime = 1e3 * ((0:size(thisWaveform, 2) - 1) / ephys_sample_rate);
+    % time
+    wvTime = 1e3 * ((0:size(thisWaveform, 2) - 1) / ephys_sample_rate);
 
-% Compute Half-Width
+    % Compute Half-Width
 
-troughAmplitude = thisWaveform(troughLocs(1));
-halfAmplitude = troughAmplitude / 2;
-aboveHalfIndices = find(thisWaveform >= halfAmplitude);
-halfWidthStartIndex = aboveHalfIndices(find(aboveHalfIndices < peakLocs(end), 1, 'last'));
-halfWidthEndIndex = aboveHalfIndices(find(aboveHalfIndices > peakLocs(end), 1));
-halfWidth = wvTime(halfWidthEndIndex) - wvTime(halfWidthStartIndex);
-if isempty(halfWidth)
-    halfWidth = NaN;
-end
-% peak to trough ratio
-peakAmplitude = thisWaveform(peakLocs(end));
-peakTroughRatio = abs(peakAmplitude/troughAmplitude);
+    troughAmplitude = thisWaveform(troughLocs(1));
+    halfAmplitude = troughAmplitude / 2;
+    aboveHalfIndices = find(thisWaveform >= halfAmplitude);
+    halfWidthStartIndex = aboveHalfIndices(find(aboveHalfIndices < peakLocs(end), 1, 'last'));
+    halfWidthEndIndex = aboveHalfIndices(find(aboveHalfIndices > peakLocs(end), 1));
+    halfWidth = wvTime(halfWidthEndIndex) - wvTime(halfWidthStartIndex);
+    if isempty(halfWidth)
+        halfWidth = NaN;
+    end
+    % peak to trough ratio
+    peakAmplitude = thisWaveform(peakLocs(end));
+    peakTroughRatio = abs(peakAmplitude/troughAmplitude);
 
-% 1rst peak to trough ratio
-firstPeakAmplitude = max(thisWaveform(1:troughLocs(1)));
-firstPeakTroughRatio = abs(firstPeakAmplitude/troughAmplitude);
+    % 1rst peak to trough ratio
+    firstPeakAmplitude = max(thisWaveform(1:troughLocs(1)));
+    firstPeakTroughRatio = abs(firstPeakAmplitude/troughAmplitude);
 
-% % Compute Rise Time
-% riseTime = time(peakIndex) - time(halfWidthStartIndex);
-% 
-% % Compute Decay Time
-% decayTime = time(halfWidthEndIndex) - time(peakIndex);
-% 
-% % Compute Rise Slope (Max Slope during the Rising Phase)
-% riseSlope = max(diff(thisWaveform(halfWidthStartIndex:peakIndex)) ./ diff(time(halfWidthStartIndex:peakIndex)));
-% 
-% % Compute Decay Slope (Max Slope during the Falling Phase)
-% decaySlope = min(diff(thisWaveform(peakIndex:halfWidthEndIndex)) ./ diff(time(peakIndex:halfWidthEndIndex)));
+    % % Compute Rise Time
+    % riseTime = time(peakIndex) - time(halfWidthStartIndex);
+    %
+    % % Compute Decay Time
+    % decayTime = time(halfWidthEndIndex) - time(peakIndex);
+    %
+    % % Compute Rise Slope (Max Slope during the Rising Phase)
+    % riseSlope = max(diff(thisWaveform(halfWidthStartIndex:peakIndex)) ./ diff(time(halfWidthStartIndex:peakIndex)));
+    %
+    % % Compute Decay Slope (Max Slope during the Falling Phase)
+    % decaySlope = min(diff(thisWaveform(peakIndex:halfWidthEndIndex)) ./ diff(time(peakIndex:halfWidthEndIndex)));
 else
     halfWidth = NaN;
     peakTroughRatio = NaN;
